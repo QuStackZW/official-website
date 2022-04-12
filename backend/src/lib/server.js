8794be940df3810d98c2fa64c4b4998d286103bc
Google Jr <andilembele020@gmail.com>
2022-04-12 22:24:41 +0200

    trying to get mongodb to run #2

--- backend/src/lib/server.js
+++ backend/src/lib/server.js
@@ -1,44 +1,50 @@
-'use strict'
+"use strict";
 
-import cors from 'cors'
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { bodyParser } from "body-parser";
 
-const app = express()
-const router = express.Router()
+const app = express();
+const port = process.env.PORT || 5000;
 
 // env vars
-const {
-    PORT,
-    MONGO_URI,
-    CORS_ORIGIN
-} = process.env
+const { PORT, MONGO_URI, CORS_ORIGIN } = process.env;
 
-mongoose.Promise = global.Promise
-mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+mongoose.Promise = global.Promise; //can be just Promise
+// const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-app.use(bodyParser.json(), cors({ origin: CORS_ORIGIN }))
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
-app.use(require('../route/auth-router'));
+app.use(bodyParser.json(), cors({ origin: CORS_ORIGIN }));
 
-app.all('*', (req, res) => {
-    res.status(404).send({
-        message: 'Not Found'
-    })
-})
+app.use(require("../route/auth-router"));
+
+app.all("*", (req, res) => {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
 
 //error middleware
-app.use(require('./error-middleware'));
+app.use(require("./error-middleware"));
 
 export const start = () => {
-    app.listen(PORT, () => {
-        console.log(`Server started on port ${PORT}`)
-    })
-}
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+};
 
 export const stop = () => {
-    app.close(() => {
-        console.log('Server stopped')
-    })
-}
+  app.close(() => {
+    console.log("Server stopped");
+  });
+};
