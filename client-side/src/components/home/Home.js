3f0d93ffb289d504250d05c7efd8bf2cc43bb3a2
Google Jr <andilembele020@gmail.com>
2022-04-20 12:33:07 +0200

    Material UI

---
+++ client-side/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Container from "@mui/material/Container";
+import NavAppBar from "../layout/NavAppBar";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <NavAppBar />
+    </React.Fragment>
+  );
+}
+
+export default Home;
--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
-import createRoot from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 
 // create container
 const rootNode = document.getElementById("root");
 const root = createRoot(rootNode);
 
+// Notes to developers:
+// 1. The createRoot function is a React utility function that creates a container for the React tree.
+// 2. The createRoot function takes a DOM node as an argument.
+// 3. The createRoot function returns a React root object.
+// 4. The createRoot function is used to create a container for the React tree.
+
+// We shall render everything under the Provider class component which is a wrapper for the entire application.
+// The Provider class component is a wrapper for the entire application.
+// The Provider class component is used to provide the application with the store.
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 serviceWorker.unregister();
